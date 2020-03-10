45c461c8a30bf71df24e3371cec47ae6d1684f06
mbagolor <mbagolor@hawaii.edu>
2020-03-10 08:08:52 -1000

    editing toggles

---
+++ art128/base-site-togglenav/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener ('DOMComponent', function(){
+
+    // TOGGLE NAV
+    var myMenuButton = document.querySelector('.menu-button');
+    var myMenuButton = document.querySelector('.menu-button');
+
+    myMenuButton.addEventListener('click', function(){
+        mytogglemenu.classList.toggle('expanded')
+    });
+
+
+});
+
+   
